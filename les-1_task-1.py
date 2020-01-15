7c7bccea8255be6074dd5883feb51fa4a1a0fc5c
Alexey Filin <falexey69@gmail.com>
2020-01-15 21:27:12 +0300

    Homework done!

---
+++ les-1_task-1.py
@@ -0,0 +1,15 @@
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+your_name = input('What is your name? - ')
+your_birth_year = int(input('What year were you born in? - '))
+print(f'So, you are {your_name} and you are {2019 - your_birth_year} years old. Nice to meet you!')
+
+birth_year = 1987
+age = 2019 - birth_year
+name = 'Alexey'
+print(f"And I'm {name}, {age} years old")
+
+# Задание можно выполнять бесконечно. К сожалению, с фантазией туго,
+# но думаю тему создания переменных, присвоения им значений,
+# запроса значений у пользователя и вывода всего этого на экран я понял.
