d448fcb8fc8556a2db8dcdb6a92fa5085c6c3cbe
Alexey Filin <falexey69@gmail.com>
2020-01-24 12:35:50 +0300

    tried to finish my HW

--- lesson-2/les-2_task-4.py
+++ lesson-2/les-2_task-4.py
@@ -3,8 +3,10 @@
 # Если в слово длинное, выводить только первые 10 букв в слове.
 
 list = list(input('Enter several words spaced: ').split(' '))
-count = 1
-for item in list:
-    print(f'{count}. {item}')
-    count += 1
+# count = 1
+# for item in list:
+#     print(f'{count}. {item}')
+#     count += 1
 
+for index, item, in enumerate(list, 1):
+    print(index, item)
